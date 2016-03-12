c75f93633cc0b999b451f1797d7a2152b3bf1642
Glenn Nagel <glenn@mercury-wireless.com>
2016-03-12 13:25:06 -0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Create a new component.
+// This component should produce some html.
 const App = () => {
-  return <div>Hello world!</div>;
+  return <div>H1!</div>;
 };
 
+// Takes this component's generated HTML and out it on the page (in the DOM)
 ReactDOM.render(
-  <App/>, document.querySelector(".container"));
+  <App/>, document.querySelector('.container'));
